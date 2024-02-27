5bfc47b6d625f30e0ec5760be09c4cc324f4d5ab
davib <davi.brantes@gmail.com>
2024-02-27 15:50:36 -0300

    style: iniciando estilização de responsividade no segmento inicial para

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
       colors: {
         "hlGreen": "#42C0B6",
         "hlBlue": "#003262",
-
+      },
+      height:{
+        "732": "732px",
+        "366": "366px"
+      },
+      fontSize:{
+        "40": "40px"
       }
     },
   },
